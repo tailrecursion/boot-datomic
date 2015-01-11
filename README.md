b43ca96cf3886a219ba37486857bd9db21b8e62c
jumblerg <jumblerg@gmail.com>
2015-01-11 00:04:24 -0500

    add log-dir option

--- README.md
+++ README.md
@@ -3,13 +3,14 @@ run the datomic transactor embedded in a boot pod
 
 ## setup
 
-install the datomic transactor pro jar into your local maven repository
+install the datomic transactor pro jar into your local maven repository.
 ```bash
 mvn install:install-file -DgroupId=com.datomic -DartifactId=datomic-transactor-pro -Dfile=datomic-transactor-pro-0.9.5078.jar -DpomFile=pom.xml
 ```
 
-add a datomic license key to your bash profile
+configure the BOOT_JVM_OPTIONS and DATOMIC LICENSE KEY environment variables. 
 ```bash
+export BOOT_JVM_OPTIONS="-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xmx2g -XX:MaxPermSize=128m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xverify:none"
 export DATOMIC_LICENSE_KEY=<license-key>
 ```
 
--- rsc/tailrecursion/boot_datomic.clj
+++ rsc/tailrecursion/boot_datomic.clj
@@ -1,5 +1,5 @@
 (ns tailrecursion.boot-datomic
-  (:require 
+  (:require
     [boot.core :as core]
     [boot.pod  :as pod] ))
 
@@ -11,24 +11,6 @@
   :memory-index-threshold "32m"
   :object-cache-max       "128m" })
 
-(defn make-pod-from-env [& artifacts]
-  (-> (core/get-env) 
-      (update-in [:dependencies] (partial filterv (fn [[a]] (some #{a} artifacts))))
-      pod/make-pod 
-      future ))
-
-(core/deftask initialize
-
-  "Initialize the database."
-
-  [u uri URI            str  "URI of the transactor."
-   t transactions EDN #{edn} "Data to initialize the database with." ]
-
-   (let [pod (make-pod-from-env 'com.datomic/datomic-pro 'ch.qos.logback/logback-classic) ]
-      (core/with-post-wrap _
-        (pod/with-call-in @pod
-          (tailrecursion.boot-datomic.peer/initialize ~*opts*) ))))
-
 (core/deftask datomic
 
   "Start the transactor.
@@ -38,17 +20,22 @@
   Memory settings default to the recommended settings for 1G of RAM appropriate for a laptop.
   Reference http://docs.datomic.com/capacity.html."
 
-  [k license-key KEY              str "Required license key."
+  [k license-key KEY              str "License key (required)."
    r protocol PROTOCOL            str "Storage protocol (dev)."
    u host HOST                    str "Connection host (localhost)."
    p port PORT                    str "Connection port (4334)."
+   l log-dir PATH                 str "The directory the logger will write to (/log)."
    t memory-index-threshold BYTES str "Threshold at which to start indexing (32m)."
    m memory-index-max BYTES       str "Maximum size of the memory index (256m)."
    c object-cache-max BYTES       str "Size of the object cache (128m)." ]
 
-   (let [pod   (make-pod-from-env 'com.datomic/datomic-transactor-pro 'ch.qos.logback/logback-classic)
-         props (merge default-opts *opts*) ]
+   (let [opts (merge default-opts *opts*)
+         jars '[com.datomic/datomic-transactor-pro ch.qos.logback/logback-classic]
+         pod  (-> (core/get-env) 
+                  (update-in [:dependencies] (partial filterv (fn [[a]] (some #{a} jars))))
+                  pod/make-pod 
+                  future ) ]
       (core/with-pre-wrap fileset
         (pod/with-call-in @pod
-          (datomic.transactor/run ~props "datomic boot task options") )
+          (datomic.transactor/run ~opts "datomic boot task options") )
         fileset )))
